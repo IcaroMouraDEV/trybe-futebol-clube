c947e9983c352c895e3110d1aea310815f8de1d2
Icaro Joel <icaromoura.programador@gmail.com>
2022-11-22 16:31:58 -0300

    Users and Logins req 01

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,24 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class UserModel extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+  userName: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default UserModel;
